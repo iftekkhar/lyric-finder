0db9a80992ba4f33cbbba57345be520e866d06ba
Iftekhar Anam <iftekhar_anam@hotmail.com>
2020-08-18 09:45:06 +0600

    final

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-
-
+//Search result
 function getSearchResult() {
-        clearSearch();
-        const searchKeyword = document.getElementById('search-input').value
-        fetch(`https://api.lyrics.ovh/suggest/${searchKeyword}`)
-        .then(res => res.json())
-                .then(data => {  
-                        storedData = data;
-            console.log(data);
-                for (let i = 0, j= 0; i < data.data.length && i < 10, j < 10; i++, j++) {
-                        const title = data.data[i].title;
-                        const preview = data.data[i].preview;
-                        const artistName = data.data[i].artist.name;
-                        const id = data.data[i].id;
-                        const albumCover = data.data[i].album.cover_xl;
-                        const albumTitle = data.data[i].album.title;
-                        let lyrics= getLyrics(artistName,title);
-                        
-                        document.getElementById('search-results').innerHTML += 
-                                                                `
+  clearSearch();
+  const searchKeyword = document.getElementById("search-input").value;
+  fetch(`https://api.lyrics.ovh/suggest/${searchKeyword}`)
+    .then((res) => res.json())
+    .then((data) => {
+      storedData = data;
+      console.log(data);
+      for (let i = 0; i < data.data.length && i < 10; i++) {
+        const title = data.data[i].title;
+        const preview = data.data[i].preview;
+        const artistName = data.data[i].artist.name;
+        const id = data.data[i].id;
+        const albumCover = data.data[i].album.cover_xl;
+        const albumTitle = data.data[i].album.title;
+
+        document.getElementById("search-results").innerHTML += `
                                                                 <div class="single-result row align-items-center my-3 p-3 d-flex justify-content-center">
                                                                     <div class="col-md-2">
                                                                         <img style="border-radius: 10px;" src="${albumCover}" alt="${albumTitle}" width="100%">
@@ -33,46 +30,38 @@ function getSearchResult() {
                                                                     </a>
                                                                          
                                                                     </div>
-                                                                    <audio class=" col-md-8 align-items-center p-2" controls>
+                                                                    <audio class="col-md-8 align-items-center p-2" controls>
                                                                         <source src="${preview}" type="audio/mpeg">
                                                                         </audio>
                                                                 </div>
                                                               
                                                                         `;
-                        
-                                                                
-                
-            }
-        })
-    }
-    
-function clearSearch() {
-        document.getElementById('search-results').innerHTML = '';  
-        document.getElementById('all-lyrics').innerHTML = '';  
+      }
+    });
 }
-
+//Clear Function
+function clearSearch() {
+  document.getElementById("search-results").innerHTML = "";
+  document.getElementById("all-lyrics").innerHTML = "";
+}
+//get lyrics search results
 function getLyrics(id) {
-
-                for (let i = 0; i < 10; i++) {
-                        if(storedData.data[i].id == id){
-                            const artistName = storedData.data[i].artist.name;
-                            const songTitle = storedData.data[i].title;
-                            fetch(`https://api.lyrics.ovh/v1/${artistName}/${songTitle}`)
-                            .then(res => res.json())
-                            .then(data => {
-                                let lyrics = data.lyrics;
-                                if(lyrics == undefined){
-                                    lyrics = `Not Found. Please Try Another one`;
-                                }
-                                document.getElementById('all-lyrics').innerHTML = `
-                                                                                            <h2 class="text-success mb-4">${songTitle} - ${artistName}</h2>
-                                                                                           <h5><pre style="color:white;" >${lyrics}</pre></h5> 
-                                                                                        </div>`
-                            })
-                            
-                            
-                        }
-                    
-        }
-        
+  for (let i = 0; i < 10; i++) {
+    if (storedData.data[i].id == id) {
+      const artistName = storedData.data[i].artist.name;
+      const songTitle = storedData.data[i].title;
+      fetch(`https://api.lyrics.ovh/v1/${artistName}/${songTitle}`)
+        .then((res) => res.json())
+        .then((data) => {
+          let lyrics = data.lyrics;
+          if (lyrics == undefined) {
+            lyrics = `Not Found. Please Try Another one`;
+          }
+          document.getElementById("all-lyrics").innerHTML = `
+<h2 class="text-success mb-4">${songTitle} - ${artistName}</h2>
+<h5><pre style="color:white;" >${lyrics}</pre></h5> 
+ </div>`;
+        });
     }
+  }
+}
